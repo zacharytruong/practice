d20005a993f6af5ec2c7f9b191a77915d5a8d7d7
Zachary Truong <zacharytruong@gmail.com>
2021-09-18 15:51:11 -0500

    add new app.js

--- scripts/script.js
+++ scripts/script.js
@@ -14,23 +14,3 @@ function resetPara(){
     para1.innerHTML = '';
     para2.innerHTML = '';
 }
-
-
-
-
-
-
-const removeFromArray = function(arr1, num0, num1 = '', num2 = '', num3 = '') {
-    const arr2 = [num0, num1, num2, num3];
-    arr2.forEach(compareArray);
-    function compareArray(arr2Value){
-        arr1.forEach(removeValue);
-        function removeValue(arr1Value, arr1Index){
-            if (arr2Value === arr1Value){
-                arr1.splice(arr1Index, 1);
-            }
-        }    
-    }
-    return arr1;
-}
-removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
