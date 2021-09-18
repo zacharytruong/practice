1154a6b1a2ac5a583b6072c6fcbf3eafec2adea5
Zachary Truong <zacharytruong@gmail.com>
2021-09-18 14:00:00 -0500

    Basic styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,22 @@
-const removeFromArray = function() {
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+let para1 = document.getElementById('demo1');
+let para2 = document.getElementById('demo2');
 
-};
+btn2.onclick = resetPara;
+function resetPara(){
+    para1.innerHTML = '';
+    para2.innerHTML = '';
+}
 
 
 
+
+
+
+const removeFromArray = function(arr1, num1, num2 = '') {
+    const arr2 = [num1, num2];
+    
+}
 
 removeFromArray();
