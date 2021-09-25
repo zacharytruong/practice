ef1724172a174b7174770824a6c5dba7a20a7682
Zachary Truong <zacharytruong@gmail.com>
2021-09-24 19:33:59 -0500

    update newest

---
+++ scripts/app copy.js
@@ -0,0 +1,11 @@
+const fruits = ['apple', 'banana', 'grapes', 'mango', 'orange']
+
+/**
+ * Filter array items based on search criteria (query)
+ */
+const filterItems = (arr, query) => {
+  return arr.filter(el => el.indexOf(query) !== -1)
+}
+
+console.log(filterItems(fruits, 'ap'))  // ['apple', 'grapes']
+console.log(filterItems(fruits, 'an'))  // ['banana', 'mango', 'orange']
